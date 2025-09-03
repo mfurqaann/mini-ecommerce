b8b9e0b334fe9de2bc9ac9c4420bec5b70e75623
mfurqaann <furqan1061@gmail.com>
2025-09-03 14:20:26 +0700

    feat(mini-ecommerce): create home page and filter product

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,27 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router";
 
 function Navbar() {
   return (
     <nav className="w-full bg-white shadow-md">
       <div className="container mx-auto px-6 py-3 flex items-center justify-between">
-        <div to="/" className="text-2xl font-bold text-gray-800">
+        <Link to="/" className="text-2xl font-bold text-gray-800">
           ShopNow
-        </div>
+        </Link>
 
         <div className="hidden md:flex space-x-8 text-gray-600 font-medium">
-          <div className="hover:text-black transition-colors">Home</div>
-          <div className="hover:text-black transition-colors">Products</div>
-          <div className="hover:text-black transition-colors">About</div>
-          <div className="hover:text-black transition-colors">Contact</div>
+          <Link className="hover:text-black transition-colors">Home</Link>
+          <Link className="hover:text-black transition-colors">Products</Link>
+          <Link className="hover:text-black transition-colors">About</Link>
+          <Link className="hover:text-black transition-colors">Contact</Link>
         </div>
         <div>
-          <div>
+          <Link to="/cart">
             <Button variant="outline" className="flex items-center gap-2">
               <ShoppingCart size={20} />
               <span>Cart (0)</span>
             </Button>
-          </div>
+          </Link>
         </div>
       </div>
     </nav>
---
+++ src/components/product/ProductFilter.jsx
@@ -0,0 +1,106 @@
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { Slider } from "@/components/ui/slider";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const items = [
+  {
+    id: "men's clothing",
+    label: "Men's Clothing",
+  },
+  {
+    id: "electronics",
+    label: "Electronics",
+  },
+  {
+    id: "women's clothing",
+    label: "Women's clothing",
+  },
+  {
+    id: "jewelery",
+    label: "Jewelery",
+  },
+];
+
+function ProductFilter({
+  handleCheckBoxFilter,
+  maxPrice,
+  rangePrice,
+  handleSlider,
+  handleSort,
+  sortedBy,
+  orderBy,
+  handleOrder,
+}) {
+  return (
+    <div className="w-full h-fit p-3 mt-8 border-2 border-solid">
+      <div className="font-bold">Filters</div>
+      <div className="mt-3">
+        <div className="font-bold">Category</div>
+        {items.map((item) => (
+          <div key={item.id} className="flex items-center gap-3 mt-3">
+            <Checkbox
+              id={item.id}
+              name={item.id}
+              onCheckedChange={() => handleCheckBoxFilter(item.id)}
+            />
+            <Label htmlFor={item.id}> {item.label} </Label>
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-5">
+        <div className="font-bold">Maximal Price</div>
+        <div>Rp {rangePrice.toLocaleString("id-ID")}</div>
+        <div className="px-3 mt-3">
+          <Slider
+            max={maxPrice}
+            step={1}
+            onValueChange={(val) => handleSlider(val)}
+          />
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <div className="font-bold">Sort by</div>
+        <div className="px-3 mt-3">
+          <Select onValueChange={(val) => handleSort(val)} value={sortedBy}>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select Sort" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="name">Name</SelectItem>
+              <SelectItem value="price">Price</SelectItem>
+              <SelectItem value="rate">Rate</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="mt-3 font-bold">Order by</div>
+        <RadioGroup
+          value={orderBy}
+          defaultValue="ASC"
+          className="flex gap-3 mt-3"
+          onValueChange={(val) => handleOrder(val)}
+        >
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="asc" id="asc" />
+            <Label htmlFor="asc">ASC</Label>
+          </div>
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="desc" id="desc" />
+            <Label htmlFor="desc">DESC</Label>
+          </div>
+        </RadioGroup>
+      </div>
+    </div>
+  );
+}
+
+export default ProductFilter;
